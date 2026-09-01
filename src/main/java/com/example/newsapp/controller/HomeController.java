97b0dbc9bdad9f9d6a21ae0011edde4addfa4939
thathsara <wthathsaradinuwan@gmail.com>
2026-09-01 13:15:49 +0530

    update the homepage

--- src/main/java/com/example/newsapp/controller/HomeController.java
+++ src/main/java/com/example/newsapp/controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.example.newsapp.controller;
 
+import com.example.newsapp.service.CategoryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private final CategoryService categoryService;
+
+    public HomeController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "index";
     }
 }
